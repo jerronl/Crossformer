d91f46d275e3b77a8d0fd1f13ac9e8ff8cd4d234
jerron <jerron@gmail.com>
2024-03-03 17:21:58 -0500

    e

--- data/data_loader.py
+++ data/data_loader.py
@@ -26,7 +26,7 @@ def cyclic_encode(original, period):
     ]
 
 def cyclic_t(x):
-    assert np.issubdtype(x.dtype, np.floating)
+    assert np.issubdtype(x.dtype, float)
     
     t = np.vectorize(lambda x: (epoch0 + timedelta(days=x)).timetuple())(x)
     tm_yday = cyclic_encode(
